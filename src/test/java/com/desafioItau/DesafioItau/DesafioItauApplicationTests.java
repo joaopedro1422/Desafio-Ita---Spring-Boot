2afa7d6f2fa0b91d8e252b9b124fa0f556ef5ef4
joaopedro1422 <jpcros40414@gmail.com>
2025-09-17 09:57:17 -0300

    iniciando atividade

---
+++ src/test/java/com/desafioItau/DesafioItau/DesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioItau.DesafioItau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
